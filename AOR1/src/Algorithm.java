b3cfcaee30dc07283dee8d6a64d63ee199d87d13
martina-zxy <martina.megasari@gmail.com>
2016-10-21 15:38:22 +0200

    tuning

--- AOR1/src/Algorithm.java
+++ AOR1/src/Algorithm.java
@@ -2,9 +2,9 @@
 public class Algorithm {
 	/* GA parameters */
     private static final double uniformRate = 0.5;
-    private static final double mutationRate = 0.03;
+    private static final double mutationRate = 0.5;
     private static final int tournamentSize = 5;
-    private static final boolean elitism = false;
+    private static final boolean elitism = true;
     
  // Evolve a population
     public static Population evolvePopulation(Population pop) {
